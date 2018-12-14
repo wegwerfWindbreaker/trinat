eebde5eef1e8fc91bc3110fcfd9bda2eba278017
me <me@10.30.223.159>
2018-12-14 09:19:35 +0100

    Animation

--- src/animation/Animation.java
+++ src/animation/Animation.java
@@ -28,11 +28,15 @@ public class Animation extends Application {
 		int velocity = (int) (Math.random()*4000 + 1000);
 		
 		return velocity;
-		
-	
-	
-		
 	}
+		
+	public int setbutton() {
+		
+		int buttons = (int) (Math.random()*8);
+		
+		return buttons;
+	}
+	
 
 	@Override
 	public void start(Stage stage) {
@@ -41,38 +45,39 @@ public class Animation extends Application {
 		Scene scene = new Scene(root, 800, 600, Color.WHITE);
 		stage.setScene(scene);
 		stage.show();
-
+		
+		for (int i=0; i<setbutton(); i++) {
 		Button button = new Button("click me");
 		button.relocate(30,setcordinate());
 		root.getChildren().add(button);
+		}
 
-		Rectangle rect = new Rectangle(100, 40, 100, 100);
-		rect.setArcHeight(50);
-		rect.setArcWidth(50);
-		rect.setFill(Color.VIOLET);
-		root.getChildren().add(rect);
-
-		TranslateTransition tt = new TranslateTransition(Duration.millis(2000), rect);
-		tt.setByX(200f);
-		tt.setCycleCount(4);
-
-		tt.setAutoReverse(true);
-		
-		// gibt dem button eine bewegung
-		TranslateTransition but = new TranslateTransition(Duration.millis(setvelocity()), button);
-		but.setByX(800f);
-//		but.setByY(200f);
-		but.setCycleCount(5);
-		but.setAutoReverse(true);
-
-		but.play();
-		tt.play();
-		
-		// gibt dem button eine reaction
-		button.setOnAction((event) -> {
-			Rectangle ract = new Rectangle(30, 40 ,50 ,60);
-			root.getChildren().add(ract);
-		});
+////		// gibt dem button eine bewegung
+////		TranslateTransition but = new TranslateTransition(Duration.millis(setvelocity()), button);
+////		but.setByX(800f);
+//////		but.setByY(200f);
+////		but.setCycleCount(5);
+////		but.setAutoReverse(true);
+////
+////		but.play();
+////
+////		// gibt dem button eine reaction
+////		button.setOnAction((event) -> {
+////			Rectangle ract = new Rectangle(30, 40 ,50 ,60);
+////			root.getChildren().add(ract);
+//		});
 	}
+
+//	Rectangle rect = new Rectangle(100, 40, 100, 100);
+//	rect.setArcHeight(50);
+//	rect.setArcWidth(50);
+//	rect.setFill(Color.VIOLET);
+//	root.getChildren().add(rect);
+//
+//	TranslateTransition tt = new TranslateTransition(Duration.millis(2000), rect);
+//	tt.setByX(200f);
+//	tt.setCycleCount(4);
+//	tt.setAutoReverse(true);
+//	tt.play();
 
 }
