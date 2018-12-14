0562a25cd16d0d2dfc29fcbce9a6435b0d6a0f60
me <me@10.30.223.159>
2018-12-14 11:07:30 +0100

    Animation

--- src/animation/Animation.java
+++ src/animation/Animation.java
@@ -3,10 +3,12 @@ package animation;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.shape.*;
 import javafx.util.Duration;
+
+
 import javafx.animation.*;
 import javafx.scene.layout.Pane;
 
@@ -14,6 +16,8 @@ public class Animation extends Application {
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
+	
+	
 	
 	// random funktion für y kordinaten
 	public double setcordinate() {
@@ -25,59 +29,70 @@ public class Animation extends Application {
 	// random funktion für geschwindigkeit
 	public int setvelocity() {
 		
-		int velocity = (int) (Math.random()*4000 + 1000);
+		int velocity = (int) (Math.random()*6000 + 1000);
 		
 		return velocity;
 	}
-		
-	public int setbutton() {
-		
-		int buttons = (int) (Math.random()*8);
-		
-		return buttons;
-	}
 	
+	// erstellen eines counters und einer anzeige
+	int counter = 1;
+	public Label label = new Label("Count: ");
+	
+	public void counter() {
+		counter ++;
+	}
+	// erstellen eines timers
+	public Label timer = new Label("Time: ");
+	
+
+
+
+
 
 	@Override
 	public void start(Stage stage) {
-		stage.setTitle("Lueg emol Michi du Opfer");
+		stage.setTitle("Mohrhuhn");
 		Pane root = new Pane();
-		Scene scene = new Scene(root, 800, 600, Color.WHITE);
+		Scene scene = new Scene(root, 1200, 1000, Color.WHITE);
 		stage.setScene(scene);
 		stage.show();
 		
-		for (int i=0; i<setbutton(); i++) {
+//		Pane pane = new Pane();
+//		pane.setPrefSize(300, 300);
+		
+		
+		root.getChildren().add(label);
+		label.relocate(30, 30);
+		
+		
+		root.getChildren().add(timer);
+		timer.relocate(100, 30);
+		
+		
 		Button button = new Button("click me");
 		button.relocate(30,setcordinate());
 		root.getChildren().add(button);
-		}
+		
 
-////		// gibt dem button eine bewegung
-////		TranslateTransition but = new TranslateTransition(Duration.millis(setvelocity()), button);
-////		but.setByX(800f);
-//////		but.setByY(200f);
-////		but.setCycleCount(5);
-////		but.setAutoReverse(true);
-////
-////		but.play();
-////
-////		// gibt dem button eine reaction
-////		button.setOnAction((event) -> {
-////			Rectangle ract = new Rectangle(30, 40 ,50 ,60);
-////			root.getChildren().add(ract);
-//		});
+		// gibt dem button eine bewegung
+		TranslateTransition but = new TranslateTransition(Duration.millis(setvelocity()), button);
+		but.setByX(1100f);
+//		but.setByY(200f);
+//		but.setCycleCount(5); ist nicht nötig es gibt nur einen cycle
+		but.setAutoReverse(true);
+		
+	
+		but.play();
+
+		// gibt dem button die gewünschten reaktionen beim draufklicken
+		button.setOnAction((event) -> {
+		       label.setText("Count: "+Integer.toString(counter));
+		       counter();;
+		 button.setDisable(true);
+		 button.setVisible(false);
+		});
+		
+		
+		
 	}
-
-//	Rectangle rect = new Rectangle(100, 40, 100, 100);
-//	rect.setArcHeight(50);
-//	rect.setArcWidth(50);
-//	rect.setFill(Color.VIOLET);
-//	root.getChildren().add(rect);
-//
-//	TranslateTransition tt = new TranslateTransition(Duration.millis(2000), rect);
-//	tt.setByX(200f);
-//	tt.setCycleCount(4);
-//	tt.setAutoReverse(true);
-//	tt.play();
-
 }
